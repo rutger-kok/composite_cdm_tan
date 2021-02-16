35233f56a04fb9f7020863120a1d0bed5b5643a8
Rutger Kok <47184115+rutger-kok@users.noreply.github.com>
2021-02-16 11:28:43 +0000

    Update composite_cdm.for
    Added license section to top of code

--- composite_cdm.for
+++ composite_cdm.for
@@ -1,10 +1,21 @@
-!----------------------------------------------------------------------!
-!     ABAQUS VUMAT USER SUBROUTINE: catalanotti_FC_3D.for              !
-!     Author(s): Rutger Kok, Francisca Martinez-Hergueta               !
-!     Last Updated: 18/01/2021                                         !
-!     Version 1.0                                                      !
-!----------------------------------------------------------------------!
+! This is a VUMAT subroutine implementing a continuum damage mechanics framework for composite materials in Abaqus.
+! Copyright (C) 2021 Rutger Wouter Kok
+! This library is free software; you can redistribute it and/or
+! modify it under the terms of the GNU Lesser General Public
+! License as published by the Free Software Foundation; either
+! version 2.1 of the License, or (at your option) any later version.
 
+! This library is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! Lesser General Public License for more details.
+
+! You should have received a copy of the GNU Lesser General Public
+! License along with this library; if not, write to the Free Software
+! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+! USA
+      
+! Last Updated: 18/01/2021                                         
 ! This is a VUMAT subroutine implementing a continuum damage mechanics
 ! framework for composite materials in Abaqus. The subroutine is based
 ! on the work of Catalanotti et al. (failure criteria) [1], Maimi
